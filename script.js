ab569581e967114a23b9e931d87691b6e637f8c5
thelowlevelguy <gkalla013@gmail.com>
2026-06-29 19:44:51 +0000

    feat: add shriking, display choices, and reset to default

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
+const playerRock = "🤜"
+const playerPaper = "🫱"
+const playerScissors = "✌️"
+
+const machineRock = '🤛🏾'
+const machinePaper = '🫲🏾'
+const machineScissors = '✌🏾'
+
 let playerScore = 0;
 let machineScore = 0;
 let playerWins = 0;
 let machineWins = 0;
 
 const buttons = document.querySelectorAll('button');
-const playerScoreBoard = document.getElementById('player_score')
-const roundResultBoard = document.getElementById('round_result')
-const machineScoreBoard = document.getElementById('machine_score')
-const playerWinsBaord = document.getElementById("player_victories")
-const machineWinsBoard = document.getElementById("machine_victories")
-const playerHands = document.getElementById("player-hands")
+const playerScoreBoard = document.getElementById('player-score')
+const roundResultBoard = document.getElementById('round-result')
+const machineScoreBoard = document.getElementById('machine-score')
+const playerWinsBaord = document.getElementById("player-victories")
+const machineWinsBoard = document.getElementById("machine-victories")
 const machineHands = document.getElementById("machine-hands") 
-
+const playerHand = document.getElementById('player-choice')
+const machineHand = document.getElementById('machine-choice')
 
 function getComputerChoice(){
 	//there are three choices and the rand is always lower than max (4)
@@ -57,25 +65,44 @@ function displayGameResult(playerScore, machineScore){
 }
 
 function handleGameHands(playerChoice, computerChoice) {
-
-	
-
-	/*for (const hand of playerHands.children){
-		if (hand.getAttribute("id").includes(playerChoice)){
-			setTimeout(() =>{
-				hand.style.hidden = false;
-			}, 400)
-		}
+	switch(playerChoice){
+	case rock:
+		playerHand.textContent = playerRock
+		break;
+	case paper:
+		playerHand.textContent = playerPaper
+		break;
+	case scissors:
+		playerHand.textContent = playerScissors
+		break;
 	}
 
-	for (const hand of machineHands.children){
-		if (hand.getAttribute("id").includes(computerChoice)){
-			setTimeout(() =>{
-				hand.style.hidden = false;
-			}, 400)
-		}
-	}*/
-	
+	switch(computerChoice){
+	case rock:
+		machineHand.textContent = machineRock
+		break;
+	case paper:
+		machineHand.textContent = machinePaper
+		break;
+	case scissors:
+		machineHand.textContent = machineScissors
+		break;
+	}
+}
+
+function shaking(){
+
+	 document.getElementById('player-choice').classList.add('shakePlayer');
+	 document.getElementById('machine-choice').classList.add('shakeMachine');
+
+	// Remove the class as soon as the animation ends
+	document.getElementById('player-choice').addEventListener('animationend', () => {
+  		document.getElementById('player-choice').classList.remove('shakePlayer');
+	});
+
+	document.getElementById('machine-choice').addEventListener('animationend', () => {
+  		document.getElementById('machine-choice').classList.remove('shakeMachine');
+	});
 }
 
 function resetGame(){
@@ -85,42 +112,39 @@ function resetGame(){
 	machineScoreBoard.textContent = machineScore;
 }
 
-function playGame(event){
-	let playerChoice = "";
-	let computerChoice = "";
-
-	playerChoice = event.target.id;
- 	computerChoice = getComputerChoice();
-	let result = playRound(playerChoice.toLowerCase(), computerChoice.toLowerCase());
-
-	if (result === "win"){
-		machineScore += 1;
-	}else if (result === "lost"){
-		playerScore += 1;
-	}
-
-	playerScoreBoard.textContent = playerScore;
-	machineScoreBoard.textContent = machineScore;
-	displayGameResult(playerScore, machineScore); 
+function setDefaultHands(){
+	playerHand.textContent = playerRock
+	machineHand.textContent = machineRock
 }
 
+function playGame(event){
 
+	let playerChoice = event.target.id;
+	let computerChoice = getComputerChoice();
+	let result = playRound(playerChoice.toLowerCase(), computerChoice.toLowerCase());
 
-buttons.forEach((button) => {
-	button.addEventListener('click', () => {
- 		 document.getElementById('player rock').classList.add('shakePlayer');
-	});
+	setDefaultHands()
+    shaking()
 
-	// Remove the class as soon as the animation ends
-	document.getElementById('player rock').addEventListener('animationend', () => {
-  		document.getElementById('player rock').classList.remove('shakePlayer');
-	});
-})
+    setTimeout(() =>{
+    	if (result === "win"){
+			machineScore += 1;
+		}else if (result === "lost"){
+			playerScore += 1;
+		}
+		handleGameHands(playerChoice, computerChoice)
+		playerScoreBoard.textContent = playerScore;
+		machineScoreBoard.textContent = machineScore;
+    }, 500)
 
+    setTimeout(() => {
+    	setDefaultHands()
+    	displayGameResult(playerScore, machineScore);
+    },1500)
+}
 
 
 buttons.forEach((button) => {
 	button.addEventListener("click", playGame)
 })
-
 
